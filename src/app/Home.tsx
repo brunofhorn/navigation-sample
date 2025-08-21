a296e5968b8f2a1101379649a8052298449bc52e
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-21 01:17:49 -0300

    feat: add bottom tab routes

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,13 +3,14 @@ import { View } from "react-native";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { ButtonIcon } from "@/components/ButtonIcon";
+import { StackRoutesProps } from "@/routes/StackRoutes";
 
-export function Home(){
-    return(
-        <View style={{flex: 1, padding: 32, paddingTop: 54}}>
+export function Home({ navigation }: StackRoutesProps<"home">) {
+    return (
+        <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
                 <Title>Home</Title>
-                <ButtonIcon name="add-circle" />
+                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: '7' })} />
             </Header>
         </View>
     )
